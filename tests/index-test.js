f7bc32863adf4eebe09e8d1158358c14a71a7057
Trent Willis <twillis@linkedin.com>
2017-05-02 13:07:02 -0700

    Add additional tests for module options

--- tests/index-test.js
+++ tests/index-test.js
@@ -6,45 +6,55 @@ const babel = require('babel-core');
 const CompactReexports = require('../lib');
 
 QUnit.module('compact-reexports', function(hooks) {
-  function transform(code, _options) {
-    const options = Object.assign({
+  function transform(code, moduleOptions) {
+    const options = {
+      moduleId: 'bar',
       plugins: [
-        'transform-es2015-modules-amd',
+        ['transform-es2015-modules-amd', moduleOptions],
         [CompactReexports]
       ]
-    }, _options);
+    };
     return babel.transform(code, options).code.trim();
   }
 
-  // TODO: Check loose option
-  // TODO: Check strict option
-  // TODO: Check noInterop option
-
   QUnit.test('correctly transforms simple re-export', function(assert) {
+    const result = transform('export { default } from "foo";');
+    assert.equal(result, 'define.alias("foo", "bar");');
+  });
+
+  QUnit.test('correctly transforms simple re-export with loose', function(assert) {
     const result = transform('export { default } from "foo";', {
-      moduleId: "bar",
+      loose: true
+    });
+    assert.equal(result, 'define.alias("foo", "bar");');
+  });
+
+  QUnit.test('correctly transforms simple re-export with strict', function(assert) {
+    const result = transform('export { default } from "foo";', {
+      strict: true
+    });
+    assert.equal(result, 'define.alias("foo", "bar");');
+  });
+
+  QUnit.test('correctly transforms simple re-export with noInterop', function(assert) {
+    const result = transform('export { default } from "foo";', {
+      noInterop: true
     });
     assert.equal(result, 'define.alias("foo", "bar");');
   });
 
   QUnit.test('correctly transforms re-export of module with more than one export', function(assert) {
-    const result = transform('export { default, helper } from "foo";', {
-      moduleId: "baz"
-    });
-    assert.equal(result, 'define.alias("foo", "baz");');
+    const result = transform('export { default, helper } from "foo";');
+    assert.equal(result, 'define.alias("foo", "bar");');
   });
 
   QUnit.test('does not transform indirect re-export', function(assert) {
-    const result = transform('import Foo from "foo"; export default Foo;', {
-      moduleId: "boo"
-    });
+    const result = transform('import Foo from "foo"; export default Foo;');
     assert.equal(result.indexOf('define.alias'), -1);
   });
 
   QUnit.test('does not transform re-exports with other code', function(assert) {
-    const result = transform('const a = "boo"; export { default } from "foo";', {
-      moduleId: "bah"
-    });
+    const result = transform('const a = "boo"; export { default } from "foo";');
     assert.equal(result.indexOf('define.alias'), -1);
   });
 });
